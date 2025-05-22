0af536ffba7638e9a6f2995cb78d2bcf456a72e0
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-05-22 11:36:00 +0530

    new

---
+++ src/main/java/com/iksen/chessCore/ChessCoreApplication.java
@@ -0,0 +1,13 @@
+package com.iksen.chessCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iksen/chessCore/ChessCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iksen.chessCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

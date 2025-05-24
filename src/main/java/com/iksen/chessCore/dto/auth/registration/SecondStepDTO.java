a5801bc572d1574766218e449ebd9f87828b9949
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-05-24 09:26:12 +0530

    new

--- src/main/java/com/iksen/chessCore/dto/auth/registration/SecondStepDTO.java
+++ src/main/java/com/iksen/chessCore/dto/auth/registration/SecondStepDTO.java
@@ -2,7 +2,8 @@ package com.iksen.chessCore.dto.auth.registration;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Email;
+
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -19,12 +20,56 @@ public class SecondStepDTO {
 
 
     @NotNull(message = "id cannot be blank")
-    @JsonProperty("id")
-    private Long id;
+    @JsonProperty("user_id")
+    private Long user_id;
 
     @NotBlank(message = "mobile cannot be blank")
     @JsonProperty("mobile")
     private String mobile;
+
+
+    private Long id;
+
+    @NotNull(message = "country_id cannot be blank")
+    @JsonProperty("country_id")
+    private Long countryId;
+
+
+    @NotNull(message = "state_id cannot be blank")
+    @JsonProperty("state_id")
+    private Long stateId;
+
+    @NotBlank(message = "first_Line cannot be blank")
+    @JsonProperty("first_line")
+    private String firstLine;
+
+    @JsonProperty("second_line")
+    private String secondLine;
     
+    @NotBlank(message = "zip cannot be blank")
+    @JsonProperty("zip")
+    private String zip;
+
+    @JsonProperty("area_no")
+    private String areaNo;
+
+    @JsonProperty("flat_no")
+    private String flatNo;
+
+
+    @JsonProperty("post")
+    private String post;
+
+    @JsonProperty("ps")
+    private String ps;
+
+    @JsonProperty("lat")
+    private String lat;
+
+    @JsonProperty("long")
+    private String longitude;
+
+    @JsonProperty("status")
+    private Integer status = 1;
 
 }
---
+++ src/main/java/com/iksen/chessCore/dto/auth/registration/UserDTO.java
@@ -0,0 +1,52 @@
+package com.iksen.chessCore.dto.auth.registration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+
+    @JsonProperty("f_name")
+    private String firstName;
+
+    @JsonProperty("l_name")
+    private String lastName;
+
+    @JsonProperty("username")
+    private String userName;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("mobile")
+    private String mobile;
+
+    @JsonProperty("avatar")
+    private String avatar;
+
+    @JsonProperty("o_auth")
+    private String oAuth;
+
+    @JsonProperty("oauth_provider")
+    private String oauthProvider;
+
+    @JsonProperty("parent_id")
+    private Long parentId = 0L;
+
+    @JsonProperty("status")
+    private Integer status = 1;
+
+    @JsonProperty("is_deleted")
+    private Integer isDeleted = 0;
+}

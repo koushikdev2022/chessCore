e07e64698d2c95f31be4acae641622b4af2d3a3f
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-06-11 17:11:33 +0530

    new

--- src/main/java/com/iksen/chessCore/controller/api/user/paymentcredential/PaymentCredController.java
+++ src/main/java/com/iksen/chessCore/controller/api/user/paymentcredential/PaymentCredController.java
@@ -35,6 +35,6 @@ public class PaymentCredController {
                                 "status_code", 400,
                                 "error",e
                              ));
-                    }
             }
+        }
 }
